b57b3eba1598b5535babdd0073c650a835b1061d
Lautaro Bachmann <laurigbach@gmail.com>
2020-12-11 16:58:03 -0300

    Created main structure for Game component

---
+++ src/components/Game.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Board from './Board'
+import Square from './Square'
+
+function Game(){
+
+    function handleClick(){
+        return            
+    }
+
+    function jumpTo(){
+        return        
+    }
+
+    function renderMoves(){
+        return        
+    }
+
+    return (
+    <Board onClick={handleClick}/>
+    )
+}
+
+export default Game
